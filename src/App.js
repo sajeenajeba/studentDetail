50ed0f846469648c2facfd652e473e6e9f32d238
Sajeena <sajeena_jebarson@mystiquex.com>
2022-06-07 10:23:56 +0530

    added validation on name field

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import StudentForm from './pages/StudentForm';
+import React from "react";
+import StudentForm from "./pages/StudentForm";
+import "./App.css";
 
 export default function App() {
   return (
@@ -8,4 +9,3 @@ export default function App() {
     </div>
   );
 }
-
--- src/pages/StudentForm.js
+++ src/pages/StudentForm.js
@@ -1,40 +1,47 @@
-import React, { useState } from 'react';
-import Grid from '@mui/material/Grid';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
-import FormLabel from '@mui/material/FormLabel';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Button from '@mui/material/Button';
-import axios from 'axios';
+import React, { useState } from "react";
+import Grid from "@mui/material/Grid";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import FormLabel from "@mui/material/FormLabel";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Button from "@mui/material/Button";
+import axios from "axios";
 
 export default function SelectOtherProps() {
-
   const data = require("../config/data");
 
-  const [studentInfo, setStudentInfo] = useState({
+  const studentInfo = {
     name: "",
     gender: "",
     date: "",
     standard: "",
     division: "",
-  });
+  };
+
+  const [formValues, setFormValues] = useState(studentInfo);
+
+  const [formErrors, setFormErrors] = useState({});
 
   const handleChange = (event) => {
-    setStudentInfo({ ...studentInfo, [event.target.name]: event.target.value });
+    const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+    console.log(formValues);
+    setFormErrors(validate(formValues));
+    event.preventDefault();
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const formData = {};
-    formData['name'] = studentInfo.name;
-    formData['gender'] = studentInfo.gender;
-    formData['date'] = studentInfo.date;
-    formData['standard'] = studentInfo.standard;
-    formData['division'] = studentInfo.division;
+    formData["name"] = formValues.name;
+    formData["gender"] = formValues.gender;
+    formData["date"] = formValues.date;
+    formData["standard"] = formValues.standard;
+    formData["division"] = formValues.division;
     console.log(formData);
 
     try {
@@ -46,10 +53,20 @@ export default function SelectOtherProps() {
       });
       console.log(response);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
+  };
 
-  }
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[a-z-\s]+$/i;
+    if (!values.name) {
+      errors.name = "Name is required!";
+    } else if (!regex.test(values.name)) {
+      errors.name = "This is not a valid format of name!";
+    }
+    return errors;
+  };
 
   return (
     <Grid
@@ -66,17 +83,19 @@ export default function SelectOtherProps() {
             label="Name"
             name="name"
             variant="outlined"
-            value={studentInfo.name}
+            value={formValues.name}
             onChange={handleChange}
-            sx={{ width: 220 }} />
+            sx={{ width: 220 }}
+          />
         </Grid>
+        <p>{formErrors.name}</p>
         <Grid item>
           <TextField
             id="date"
             label="Date of Birth"
             type="date"
             name="date"
-            value={studentInfo.date}
+            value={formValues.date}
             onChange={handleChange}
             fullWidth
             sx={{ marginTop: 5 }}
@@ -85,12 +104,13 @@ export default function SelectOtherProps() {
             }}
           />
         </Grid>
+        <p>{formErrors.date}</p>
         <Grid item>
           <TextField
             select
             label="Standard"
             name="standard"
-            value={studentInfo.standard}
+            value={formValues.standard}
             onChange={handleChange}
             fullWidth
             sx={{ marginTop: 5 }}
@@ -107,7 +127,7 @@ export default function SelectOtherProps() {
             select
             label="Division"
             name="division"
-            value={studentInfo.division}
+            value={formValues.division}
             onChange={handleChange}
             fullWidth
             sx={{ marginTop: 5 }}
@@ -121,23 +141,31 @@ export default function SelectOtherProps() {
         </Grid>
         <Grid item>
           <FormControl>
-            <FormLabel id="gender" sx={{ marginTop: 5 }}>Gender</FormLabel>
+            <FormLabel id="gender" sx={{ marginTop: 5 }}>
+              Gender
+            </FormLabel>
             <RadioGroup
               row
               aria-labelledby="gender"
               name="gender"
-              value={studentInfo.gender}
+              value={formValues.gender}
               onChange={handleChange}
             >
               <FormControlLabel value="male" control={<Radio />} label="Male" />
-              <FormControlLabel value="female" control={<Radio />} label="Female" />
+              <FormControlLabel
+                value="female"
+                control={<Radio />}
+                label="Female"
+              />
             </RadioGroup>
           </FormControl>
         </Grid>
         <Grid item>
-          <Button fullWidth variant="contained" type="submit">Submit</Button>
+          <Button fullWidth variant="contained" type="submit">
+            Submit
+          </Button>
         </Grid>
       </form>
     </Grid>
-  )
+  );
 }
